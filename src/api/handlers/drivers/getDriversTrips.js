aeec22658ef61b2f8685ee59dbcadf4bdb90b72d
malinkacat <gumnickajaliza@gmail.com>
2023-06-11 16:55:56 +0300

    update get driver's trips api

--- src/api/handlers/drivers/getDriversTrips.js
+++ src/api/handlers/drivers/getDriversTrips.js
@@ -3,37 +3,62 @@ const { Drivers, Trips, Users } = require("../../../models");
 module.exports.getDriversTrips = async (req, res) => {
   const { _id } = req.params;
   const { isActive } = req.query;
-  let trips;
+
   const driver = await Drivers.findOne({ _id: _id });
   if (!driver) {
     return res.status(400).send({ message: "Driver was not found" });
   }
+  let trips = [];
+  if (isActive == "true") {
+    trips = await Trips.find({
+      _id: { $in: driver.tripIds },
+      $or: [
+        {
+          "date.end": { $gt: new Date() },
+          "date.start": { $lt: new Date() },
+        },
+        {
+          "date.start": { $gt: new Date() },
+        },
+      ],
+    });
+  } else if (isActive == "false") {
+    trips = await Trips.find({
+      _id: { $in: driver.tripIds },
+      "date.end": { $lt: new Date() },
+    });
+  } else if (!isActive) {
+    trips = await Trips.find({
+      _id: { $in: driver.tripIds },
+    });
+  } else {
+    return res
+      .status(400)
+      .send({ message: "Wrong value of isActive. Enter true or false" });
+  }
 
-  // const filter = {};
-  // if (isActive === "active") {
-  //   filter["date.start"] = { $lt: new Date() };
-  //   filter["date.end"] = { $gt: new Date() };
-  // } else if (isActive === "archived") {
-  //   filter["date.end"] = { $lt: new Date() };
-  // }
-  trips = await Trips.find({ _id: { $in: driver.tripIds } });
-  const userIds = trips.flatMap((trip) => trip.userIds.map((user) => user._id));
+  const userIds = trips.flatMap((trip) => trip.userIds.map((elem) => elem._id));
   const users = await Users.find({ _id: { $in: userIds } });
-  //console.log(users);
-  const formattedTrips = trips.map((trip) => ({
-    date: {
-      start: trip.date.start,
-      end: trip.date.end,
-    },
-    pointA: trip.pointA,
-    pointB: trip.pointB,
-    price: trip.price,
-    users: trip.userIds.map((user) => {
+
+  const newTrips = trips.map((trip) => {
+    const tripUsers = trip.userIds.map((u) => {
+      const user = users.find(
+        (user) => user._id.toString() === u._id.toString()
+      );
       return {
-        firstName: user._id.firstName,
-        lastName: user._id.lastName,
+        firstName: user.firstName,
+        lastName: user.lastName,
       };
-    }),
-  }));
-  return res.status(200).send(formattedTrips);
+    });
+
+    return {
+      _id: trip._id,
+      date: trip.date,
+      pointA: trip.pointA,
+      pointB: trip.pointB,
+      price: trip.price,
+      users: tripUsers,
+    };
+  });
+  return res.status(200).send(newTrips);
 };
